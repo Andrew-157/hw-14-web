fa2802fc5c558d41481bc2f5d42c832fcd6c259d
Andrew Subotin <subotinandrey5@gmail.com>
2022-12-28 14:06:27 +0200

    get authors, quotes and tags

---
+++ app/app/spiders/quotes_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+
+    name = 'quotes'
+
+    allowed_domains = ['quotes.toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+
+        for quote in response.xpath("//div[@class='quote']"):
+            yield {
+                'author': quote.xpath("span/small/text()").get(),
+                'quote': quote.xpath("span[@class='text']/text()").get(),
+                'tags': quote.xpath("div[@class='tags']/a/text()").extract(),
+            }
+        next_link = response.xpath("//li[@class='next']/a/@href").get()
+        if next_link:
+            yield scrapy.Request(url=self.start_urls[0] + next_link)
